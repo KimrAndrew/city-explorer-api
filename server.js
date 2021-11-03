21eb32884a21cb599f2cd7bc293628146b0e3c65
KimrAndrew <andrewrk1997@gmail.com>
2021-11-02 20:38:44 -0700

    server working

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 'use strict';
 
 require('dotenv').config();
+class Forcast {
+    constructor(data,description) {
+        this.date = data;
+        this.description = description;
+    }
+}
+
 const express = require('express');
 const cors = require('cors');
-const weatherData = require('./data/weather.json');
+const weather = require('./data/weather.json');
 
 const app = express();
 
@@ -12,8 +19,12 @@ app.use(cors());
 const PORT = process.env.PORT || 3001
 
 app.get('/weather',handleGetWeather);
+app.get('/*', (req,res) => res.status(403).send('not found'));
 
 function handleGetWeather(req,res) {
-    console.log(req.query.name);
     console.log('Hello from weather!');
+    res.status(200).send(weather[0].city_name + " " + weather[1].city_name + " " + weather[2].city_name);
+    
 }
+
+app.listen(PORT, () => console.log(`I am a server that is listening on port:${PORT}`));

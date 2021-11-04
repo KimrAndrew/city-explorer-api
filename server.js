8b8ff10e73f222428f8bd9af77421c01964eaa11
KimrAndrew <andrewrk1997@gmail.com>
2021-11-03 22:02:47 -0700

    readded console.log

--- server.js
+++ server.js
@@ -38,6 +38,7 @@ async function handleGetWeather(req,res) {
         //console.log(weather);
         let forcasts;
         forcasts = weather.map(el => new Forcast(el.valid_date,el.weather.description,el.min_temp,el.max_temp));
+        console.log(forcasts);
         res.staus(200).send(forcasts);
     } catch {
         res.status(500).send("something went wrong");

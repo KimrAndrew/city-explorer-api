6199fdf65ab1e46c42a9d30a25d8eda9551438e3
KimrAndrew <andrewrk1997@gmail.com>
2021-11-04 18:46:45 -0700

    retrieves movie data from TMDb and sends an array of objects created using that data

--- server.js
+++ server.js
@@ -13,7 +13,46 @@ app.use(cors());
 
 const PORT = process.env.PORT || 3001
 
+
+
+async function handleGetMovies(req,res) {
+    try {
+        let city_name = req.query.city_name;
+        console.log(city_name);
+        let movies = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${city_name}`);
+        
+        //lets get the 10 most popular movies
+        let movieData = movies.data.results.sort((a,b) => {
+            if(a.popularity > b.popularity) {
+                return -1;
+            } else if (a.popularity < b.popularity) {
+                return 1;
+            } else {
+                return 0;
+            }
+        });
+
+        movieData = movies.data.results.slice(0,9);
+        movieData=movieData.map(movie => new Movie(movie));
+        console.log(movieData);
+        res.status(200).send(movieData);
+    } catch(error) {
+        console.log(error);
+        res.status(500).send(error);
+    }
+}
+
+class Movie {
+    constructor(obj) {
+        this.title = obj.title;
+        this.release_date = obj.release_date;
+        this.poster_path = obj.poster_path;
+        this.popularity = obj.popularity;
+    }
+}
+
 app.get('/weather',handleGetWeather);
+app.get('/movies',handleGetMovies);
 app.get('/*', (req,res) => res.status(404).send('Path does not exist'));
 
 
